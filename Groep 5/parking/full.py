fe421853e9a13d7b996d714643ee9839c7dd9a20
Gerben <gerbenvaneerten@gmail.com>
2018-06-14 14:02:46 +0200

    Parking analysis: data check, parked heatmap and garage full

--- Groep 5/parking/full.py
+++ Groep 5/parking/full.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import datetime as dt
+import matplotlib.pyplot as plt
 
 from parking.util import load_garage_transactions
 from parking.util import load_garages
@@ -8,6 +9,46 @@ from parking.util import timeseries_garages as timeseries
 from parking.util import fix_start_assume_first
 from parking.util import fix_end_by_timedelta
 from parking.util import mark_periods
+
+def dayhours():
+    ret = []
+    for d in range(7):
+        for h in range(24):
+            ret.append('%s-%s' % (d, h))
+    return ret
+
+def yearweeks(start, end):
+    ret = []
+    t = start
+    while t < end:
+        ret.append(t.strftime('%Y-%W'))
+        t += dt.timedelta(weeks=1)
+    return ret
+
+def yearweek_dayhour_full(ts):
+    ts['yearweek'] = ts['dt'].apply(lambda x: x.strftime('%Y-%V'))
+    ts['dayhour'] = ts['dt'].apply(lambda x: x.strftime('%w-%H'))
+    df = ts.groupby(['yearweek','dayhour'])['full'].any()
+    f = df.unstack(1).fillna(False)
+    f = f.astype(int)
+    start = ts['dt'].min()
+    end = ts['dt'].max()
+    d = pd.DataFrame(0, index=yearweeks(start,end), columns=dayhours())
+    d.update(f)
+    d = d.astype(int)
+    return d
+
+def plot_heatmap(sel):
+    plt.pcolor(sel)
+    #plt.yticks(np.arange(sel.shape[0]))
+    plt.xticks(np.arange(sel.shape[1], step=24))
+
+def save_heatmap(ts, garage_id):
+    plot_heatmap(yearweek_dayhour_full(select_garage(ts, garage_id)))
+    plt.savefig('results/parking_garage_%s_heatmap_yearweek_dayhour.png' % garage_id)
+
+def select_garage(ts, garage_id):
+    return ts[(ts['garage_id'] == garage_id)]
 
 def mark_full(ts, garages):
     ts['capacity'] = ts['garage_id'].map(garages['capacity_value'])
@@ -31,14 +72,15 @@ if __name__ == '__main__':
     print('Assume end nan after 0 hours')
     fix_end_by_timedelta(df, dt.timedelta(hours=0))
 
-    df['duration'] = df['end'] - df['start']
+    #df['duration'] = df['end'] - df['start']
 
     ts = timeseries(df)
 
     mark_full(ts, garages)
     mark_changed(ts)
-    mark_periods(ts)
+    #mark_periods(ts)
     for i,v in garages.iterrows():
         print(i)
         print(v)
         print(changes_status(ts, i))
+        save_heatmap(ts, i)
